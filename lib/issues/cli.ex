16100ede964b7bb343f48a0a9dc914bec4222cf1
Victor Landim <victorlandim5@gmail.com>
2020-09-09 17:53:54 +0200

    Add missing files.

---
+++ lib/issues/cli.ex
@@ -0,0 +1,77 @@
+defmodule Issues.Cli do
+  @default_count 4
+
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project.
+  """
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+  Otherwise it is a github user name, project name and (optionally)
+  the number of entries to format.
+  Returns a tuple of `{user, project, count}`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+
+  defp args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  defp args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  defp args_to_internal_representation(_) do
+    :help
+  end
+
+  defp process(:help) do
+    IO.puts("""
+    Usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  defp process({user, project, count}) do
+    Issues.Github.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  defp decode_response({:ok, body}), do: body
+
+  defp decode_response({:error, error}) do
+    IO.puts("Error fetching from Github, #{inspect(error["message"])}")
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn a, b -> a["created_at"] >= b["created_at"] end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
+  end
+end
